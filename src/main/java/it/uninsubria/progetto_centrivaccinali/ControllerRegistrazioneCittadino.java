7b25c061d23b145753f47d7de9c41fa5bcf84f76
cricci3 <cricci3@studenti.uninsubria.it>
2022-05-18 11:38:20 +0200

    Vista registrazione-cittadino.fxml

---
+++ src/main/java/it/uninsubria/progetto_centrivaccinali/ControllerRegistrazioneCittadino.java
@@ -0,0 +1,16 @@
+package it.uninsubria.progetto_centrivaccinali;
+
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+
+public class ControllerRegistrazioneCittadino {
+    @FXML
+    private TextField tf_nomeReg, tf_cognomeReg, tf_cfReg, tf_emailReg, tf_usernameReg, tf_idReg;
+    @FXML
+    private PasswordField pf_passwordReg;
+
+
+    public void registrazioneCittadino(ActionEvent actionEvent) {
+    }
+}
