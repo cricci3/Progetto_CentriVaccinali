e72428131b920053942e35c67709f009c7df66e6
cricci3 <cricci3@studenti.uninsubria.it>
2022-05-18 09:46:25 +0200

    finita vista add-centro-vaccinale.fxml

--- src/main/java/it/uninsubria/progetto_centrivaccinali/ControllerAddCentroVaccinale.java
+++ src/main/java/it/uninsubria/progetto_centrivaccinali/ControllerAddCentroVaccinale.java
@@ -1,4 +1,23 @@
 package it.uninsubria.progetto_centrivaccinali;
 
-public class ControllerAddCentroVaccinale {
+import javafx.fxml.*;
+import javafx.scene.control.*;
+
+import java.net.*;
+import java.util.*;
+
+public class ControllerAddCentroVaccinale implements Initializable{
+    @FXML
+    private Button btn_addCV;
+    @FXML
+    private TextField tf_nomeCV, tf_indirizzoCV, tf_comuneCV, tf_capCV, tf_provinciaCV;
+    @FXML
+    private ChoiceBox<String> cb_tipoCV;
+
+    private String[] tipologia = {"ospedale","hub","aziendale"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        cb_tipoCV.getItems().addAll(tipologia);
+    }
 }
