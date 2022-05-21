01e7deeed06fa8aed7806842b9e34b6eb29a24bf
cricci3 <cricci3@studenti.uninsubria.it>
2022-05-21 12:47:13 +0200

    back su hello-informazioni.fxml

--- src/main/java/it/uninsubria/progetto_centrivaccinali/ControllerHelloCittadino.java
+++ src/main/java/it/uninsubria/progetto_centrivaccinali/ControllerHelloCittadino.java
@@ -69,4 +69,22 @@ public class ControllerHelloCittadino {
             System.out.println( "    ----------------------------------------\n" );
         }
     }
+    public void back(ActionEvent event){
+        String sceneFile = "hello-view.fxml";
+        URL url = getClass().getResource(sceneFile);
+        try {
+            FXMLLoader fxmlLoader;
+            fxmlLoader= new FXMLLoader(url);
+            root = fxmlLoader.load();
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+            System.out.println( "Exception on FXMLLoader.load()" );
+            System.out.println( "  * url: " + url );
+            System.out.println( "  * " + e );
+            System.out.println( "    ----------------------------------------\n" );
+        }
+    }
 }
--- src/main/java/it/uninsubria/progetto_centrivaccinali/ControllerHelloInformazioni.java
+++ src/main/java/it/uninsubria/progetto_centrivaccinali/ControllerHelloInformazioni.java
@@ -2,12 +2,17 @@ package it.uninsubria.progetto_centrivaccinali;
 
 import javafx.event.*;
 import javafx.fxml.*;
+import javafx.scene.*;
 import javafx.scene.control.*;
+import javafx.stage.*;
 
 import java.net.*;
 import java.util.*;
 
 public class ControllerHelloInformazioni implements Initializable{
+    private Parent root;
+    private Scene scene;
+    private Stage stage;
     @FXML
     private TextField tf_nome, tf_comune;
 
@@ -25,5 +30,23 @@ public class ControllerHelloInformazioni implements Initializable{
     }
 
     public void cercaTipoComuneCentro(ActionEvent event) {
+    }
+    public void back(ActionEvent event){
+        String sceneFile = "hello-cittadino.fxml";
+        URL url = getClass().getResource(sceneFile);
+        try {
+            FXMLLoader fxmlLoader;
+            fxmlLoader= new FXMLLoader(url);
+            root = fxmlLoader.load();
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }catch (Exception e){
+            System.out.println( "Exception on FXMLLoader.load()" );
+            System.out.println( "  * url: " + url );
+            System.out.println( "  * " + e );
+            System.out.println( "    ----------------------------------------\n" );
+        }
     }
 }
