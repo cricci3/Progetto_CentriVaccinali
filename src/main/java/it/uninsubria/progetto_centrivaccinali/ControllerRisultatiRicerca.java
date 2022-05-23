0e152879097b5b665b3f049fb5cb967b39509a9c
cricci3 <cricci3@studenti.uninsubria.it>
2022-05-23 21:03:41 +0200

    logout su hello-operatore.fxml e login-operatore.fxml

--- src/main/java/it/uninsubria/progetto_centrivaccinali/ControllerRisultatiRicerca.java
+++ src/main/java/it/uninsubria/progetto_centrivaccinali/ControllerRisultatiRicerca.java
@@ -63,9 +63,6 @@ public class ControllerRisultatiRicerca implements Initializable {
                 }catch (Exception e){
                     e.printStackTrace();
                 }
-
-
-
             }
         });
     }
