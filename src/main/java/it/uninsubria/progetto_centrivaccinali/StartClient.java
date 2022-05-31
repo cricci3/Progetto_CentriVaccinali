57a6150f03f692bae2f0b2670621625e1f4c89ab
Alessandro Macrina <alessandro.macrina1997@gmail.com>
2022-05-31 18:09:08 +0200

    corretto StartClient.java. non viene più lanciato anche il server

--- src/main/java/it/uninsubria/progetto_centrivaccinali/StartClient.java
+++ src/main/java/it/uninsubria/progetto_centrivaccinali/StartClient.java
@@ -2,7 +2,6 @@ package it.uninsubria.progetto_centrivaccinali;
 
 public class StartClient {
     public static void main(String[] args) {
-        ServerRMI.main(args);
         HelloApplication.main(args);
     }
 }
