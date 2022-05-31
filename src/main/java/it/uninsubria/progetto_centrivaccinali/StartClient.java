29bda48a6c8ca7f7c94600d7afce4f7cd78658af
cricci3 <cricci3@studenti.uninsubria.it>
2022-05-31 17:17:15 +0200

    start client fa partire entrambe

--- src/main/java/it/uninsubria/progetto_centrivaccinali/StartClient.java
+++ src/main/java/it/uninsubria/progetto_centrivaccinali/StartClient.java
@@ -2,6 +2,7 @@ package it.uninsubria.progetto_centrivaccinali;
 
 public class StartClient {
     public static void main(String[] args) {
+        ServerRMI.main(args);
         HelloApplication.main(args);
     }
 }
